cd511ef881122e199d8ca73c90817bddc4b87965
Paulo Oyama <papaloyama@gmail.com>
2022-09-08 23:08:56 -0300

    Length of Last Word

--- Length_of_Last_Word/Length_of_Last_Word.py
+++ Length_of_Last_Word/Length_of_Last_Word.py
@@ -1,19 +1,25 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
+        if len(s) == 0:
+            return 0
+        if len(s) == 1:
+            return 1
+
         index = len(s) - 1
         i = 0
-        validator = True
 
-        while s[index] != ' ' or validator:
-            if s[index] != ' ':
-                validator = False
+        while (index > 0) and not (s[index-1] == ' ' and s[index] != ' '):
+            if s[index] == ' ':
                 i += 1
-
             index -= 1
 
-        return i
+        res = s[(index): (len(s) - i)]
+
+        return len(res)
 
 
 print(Solution().lengthOfLastWord("Hello World"))
 print(Solution().lengthOfLastWord("   fly me   to   the moon  "))
 print(Solution().lengthOfLastWord("luffy is still joyboy"))
+print(Solution().lengthOfLastWord("day"))
+print(Solution().lengthOfLastWord("a "))
